a8f342eb791e7ccecf29855bb22bebcd5fc37a6e
clxx <Carsten.Lindner@gmx.net>
2017-12-02 02:00:36 +0100

    Step 7.4

--- ColorName/Startup.cs
+++ ColorName/Startup.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace ColorName
 {
@@ -27,6 +29,15 @@ namespace ColorName
             }
 
             app.UseStaticFiles();
+
+            if (env.IsDevelopment())
+            {
+                app.UseStaticFiles(new StaticFileOptions()
+                {
+                    FileProvider = new PhysicalFileProvider(Path.GetFullPath("ClientApp")),
+                    RequestPath = new PathString("/debug")
+                });
+            }
 
             app.UseMvcWithDefaultRoute();
         }
