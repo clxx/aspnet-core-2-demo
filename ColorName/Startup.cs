b17a092ac79001de3cb6789a316b549fa42aa9f3
clxx <Carsten.Lindner@gmx.net>
2017-12-02 02:50:38 +0100

    Step 9.4

--- ColorName/Startup.cs
+++ ColorName/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 
@@ -15,12 +16,32 @@ namespace ColorName
 {
     public class Startup
     {
+        private IConfiguration _configuration;
+
+        // There is Dependency Injection everywhere!
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/?tabs=aspnetcore2x#commandline-configuration-provider
+            // E.g. 'dotnet run key1=value1'
+            // or '"args": ["db=mssql"],' in '.vscode/launch.json'.
             services.AddDbContext<ColorNameContext>(options =>
-                options.UseSqlite("Data Source=ColorName.db"));
+            {
+                if (_configuration["db"] == "mssql")
+                {
+                    options.UseSqlServer("Initial Catalog=ColorName;User Id=SA;Password=Passw0rd!");
+                }
+                else
+                {
+                    options.UseSqlite("Data Source=ColorName.db");
+                }
+            });
 
             services.AddMvc();
         }
